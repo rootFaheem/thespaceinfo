bc50bfb7460cb63926600f5a4059351cbba13768
rootFaheem <faheemtech1@gmail.com>
2021-01-21 23:44:43 +0530

    mars page arch

--- lib/app/modules/mars/mars.dart
+++ lib/app/modules/mars/mars.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 class Mars extends StatelessWidget {
   static const routeName = "mars";
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('Mars Page'),
+        Container(
+          margin: EdgeInsets.only(top: 24.0),
+          child: Text(
+            'MARS',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ),
+        Container(
+          child: Text('The Red Planet'),
+        ),
+        Container(
+          child: Text('Mars Image goes here'),
+        ),
       ],
     );
   }
