e8cabb73b97395d6d747285c0f4ed2c1a8cfba14
rootFaheem <faheemtech1@gmail.com>
2021-01-23 00:24:29 +0530

    tabs added in missions screen

--- lib/app/modules/missions/missions.dart
+++ lib/app/modules/missions/missions.dart
@@ -1,29 +1,78 @@
 import 'package:flutter/material.dart';
 
-class MissionScreen extends StatelessWidget {
+class MissionScreen extends StatefulWidget {
   static const routeName = "missions";
   @override
+  _NestedTabBarState createState() => _NestedTabBarState();
+}
+
+class _NestedTabBarState extends State<MissionScreen>
+    with TickerProviderStateMixin {
+  TabController _nestedTabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _nestedTabController = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nestedTabController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: TabBar(
-              tabs: [
-                Tab(text: 'Upcoming'),
-                Tab(text: 'Recent'),
-              ],
-            ),
-            // title: Text('Tabs Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              Container(child: Text('Upcoming')),
-              Container(child: Text('Recent ')),
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Container(
+      color: Theme.of(context).backgroundColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          TabBar(
+            controller: _nestedTabController,
+            indicatorColor: Theme.of(context).accentColor,
+            labelColor: Theme.of(context).textTheme.bodyText1.color,
+            unselectedLabelColor: Colors.white70,
+            isScrollable: true,
+            tabs: <Widget>[
+              Tab(text: "Upcoming"),
+              Tab(text: "Recent"),
             ],
           ),
-        ),
+          Container(
+            height: double.tryParse("600"),
+            // screenHeight * 0.70
+            margin: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: TabBarView(
+              controller: _nestedTabController,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Text('upcoming goes here...'),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  child: Text('recent goes here...'),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
