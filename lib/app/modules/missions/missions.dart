d74aa2a951a7dc7bff33df524650362389e31131
rootFaheem <faheemtech1@gmail.com>
2021-01-31 22:48:05 +0530

    tab issue on home page

--- lib/app/modules/missions/missions.dart
+++ lib/app/modules/missions/missions.dart
@@ -25,54 +25,55 @@ class _NestedTabBarState extends State<MissionScreen>
 
   @override
   Widget build(BuildContext context) {
-    // double screenHeight = MediaQuery.of(context).size.height;
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          TabBar(
-            controller: _nestedTabController,
-            indicatorColor: Theme.of(context).accentColor,
-            labelColor: Theme.of(context).textTheme.bodyText1.color,
-            unselectedLabelColor: Colors.white70,
-            isScrollable: true,
-            tabs: <Widget>[
-              Tab(text: "Upcoming"),
-              Tab(text: "Recent"),
-            ],
-          ),
-          Container(
-            height: double.tryParse("600"),
-            // screenHeight * 0.70
-            margin: EdgeInsets.only(left: 16.0, right: 16.0),
-            child: TabBarView(
+    double screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            TabBar(
               controller: _nestedTabController,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Theme.of(context).backgroundColor,
-                  ),
-                  child: SingleChildScrollView(
-                    child: Text('upcoming goes here...'),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Theme.of(context).backgroundColor,
-                  ),
-                  child: Text('recent goes here...'),
-                ),
+              indicatorColor: Theme.of(context).accentColor,
+              labelColor: Theme.of(context).textTheme.bodyText1.color,
+              unselectedLabelColor: Colors.white70,
+              isScrollable: true,
+              tabs: <Widget>[
+                Tab(text: "Upcoming"),
+                Tab(text: "Recent"),
               ],
             ),
-          )
-        ],
+            Container(
+              height: screenHeight * 0.85,
+              margin: EdgeInsets.only(left: 16.0, right: 16.0),
+              child: TabBarView(
+                controller: _nestedTabController,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Theme.of(context).backgroundColor,
+                    ),
+                    child: SingleChildScrollView(
+                      child: Text('upcoming goes here...'),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Theme.of(context).backgroundColor,
+                    ),
+                    child: Text('recent goes here...'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
