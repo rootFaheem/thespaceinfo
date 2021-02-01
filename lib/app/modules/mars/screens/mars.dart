9f6bfebb5bba46fbd5aff1e2385e872adec85672
rootFaheem <faheemtech1@gmail.com>
2021-02-01 23:16:18 +0530

    mars details issues

--- lib/app/modules/mars/screens/mars.dart
+++ lib/app/modules/mars/screens/mars.dart
@@ -11,41 +11,43 @@ class Mars extends StatefulWidget {
 class _MarsState extends State<Mars> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 287.0,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/mars/mars_background.png"),
-              fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 287.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/mars/mars_background.png"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 24.0, bottom: 8.0),
-                child: Text(
-                  'MARS',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w900,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 24.0, bottom: 8.0),
+                  child: Text(
+                    'MARS',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 8.0),
-                child: Text('The Red Planet'),
-              ),
-              Container(
-                child: Image.asset('assets/images/mars/mars.png'),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.only(bottom: 8.0),
+                  child: Text('The Red Planet'),
+                ),
+                Container(
+                  child: Image.asset('assets/images/mars/mars.png'),
+                ),
+              ],
+            ),
           ),
-        ),
-        MarsDetails(),
-      ],
+          MarsDetails(),
+        ],
+      ),
     );
   }
 }
