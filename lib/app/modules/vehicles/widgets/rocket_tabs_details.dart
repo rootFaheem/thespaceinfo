ded7c25c8e2d78d029834e543d8ff77867b330d0
rootFaheem <faheemtech1@gmail.com>
2021-02-02 23:33:31 +0530

    bar added to top of bottom sheet

--- lib/app/modules/vehicles/widgets/rocket_tabs_details.dart
+++ lib/app/modules/vehicles/widgets/rocket_tabs_details.dart
@@ -32,7 +32,8 @@ class _NestedTabBarState extends State<RocketTabsDetails>
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Container(
-              color: Colors.blue,
+              margin: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              color: Color(0xffC4C4C4),
               child: SizedBox(
                 height: 4.0,
                 width: 74.0,
