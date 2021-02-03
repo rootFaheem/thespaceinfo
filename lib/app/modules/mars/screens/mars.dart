7fdb41bf3ce842d10b894bc8c00b2193e13b6a15
rootFaheem <faheemtech1@gmail.com>
2021-02-03 22:34:57 +0530

    mars tab added to root mars screen

--- lib/app/modules/mars/screens/mars.dart
+++ lib/app/modules/mars/screens/mars.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../widgets/mars_details.dart';
 
 class Mars extends StatefulWidget {
   static const routeName = "mars";
@@ -8,9 +7,25 @@ class Mars extends StatefulWidget {
   _MarsState createState() => _MarsState();
 }
 
-class _MarsState extends State<Mars> {
+class _MarsState extends State<Mars> with TickerProviderStateMixin {
+  TabController _nestedTabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _nestedTabController = new TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nestedTabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -45,7 +60,78 @@ class _MarsState extends State<Mars> {
               ],
             ),
           ),
-          MarsDetails(),
+          // MarsDetails(),
+          Container(
+            color: Theme.of(context).backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                  color: Color(0xffC4C4C4),
+                  child: SizedBox(height: 4.0, width: 74.0),
+                ),
+                TabBar(
+                  controller: _nestedTabController,
+                  indicatorColor: Theme.of(context).accentColor,
+                  labelColor: Theme.of(context).textTheme.bodyText1.color,
+                  unselectedLabelColor: Colors.white70,
+                  isScrollable: true,
+                  indicatorWeight: 2.0,
+                  tabs: <Widget>[
+                    Tab(text: "Overview"),
+                    Tab(text: "Fun Facts"),
+                    Tab(text: "Perseverance Rover"),
+                    Tab(text: "Gallery"),
+                  ],
+                ),
+                Container(
+                  height: screenHeight * 0.50,
+                  margin: EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: TabBarView(
+                    controller: _nestedTabController,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                        child: SingleChildScrollView(
+                          child: Text('Overview goes here...'),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
