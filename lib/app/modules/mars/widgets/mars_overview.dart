aa5066dade933d96515aaf1cb02e28f1e48135e2
rootFaheem <faheemtech1@gmail.com>
2021-02-06 23:53:31 +0530

    data mapping with left line

--- lib/app/modules/mars/widgets/mars_overview.dart
+++ lib/app/modules/mars/widgets/mars_overview.dart
@@ -8,10 +8,29 @@ class MarsOverview extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-              child: Text(
-            "About Mars",
-            style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold),
-          ))
+            child: Text(
+              "About Mars",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 18.0),
+                color: Color(0xffC4C4C4),
+                child: SizedBox(height: 74.0, width: 4.0),
+              ),
+              Container(
+                width: 310.0,
+                margin: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  "Mars is a cold desert world. It is half the size of Earth. Mars is sometimes called the Red Planet. It\'s red because of rusty iron in the ground.",
+                  style: TextStyle(fontSize: 14.0),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
