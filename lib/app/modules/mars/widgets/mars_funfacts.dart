c00a7d9423a29f7e16c0792c049eb5dd5236cbc0
rootFaheem <faheemtech1@gmail.com>
2021-02-08 22:21:00 +0530

    fun fact more point

--- lib/app/modules/mars/widgets/mars_funfacts.dart
+++ lib/app/modules/mars/widgets/mars_funfacts.dart
@@ -31,6 +31,30 @@ class MarsFunFact extends StatelessWidget {
               ),
             ],
           ),
+          Container(
+            child: Text(
+              "Tallest Mountain",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 18.0),
+                color: Color(0xffC4C4C4),
+                child: SizedBox(height: 74.0, width: 4.0),
+              ),
+              Container(
+                width: 310.0,
+                margin: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  "Mars is home to the tallest mountain in the solar system. Olympus Mons, a shield volcano, is 21km high and 600km in diameter. Despite having formed over billions of years, evidence from volcanic lava flows is so recent many scientists believe it could still be active.",
+                  style: TextStyle(fontSize: 14.0),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
