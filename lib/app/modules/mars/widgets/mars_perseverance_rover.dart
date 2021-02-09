13e0da12f74e5edb457c0fc64fdc8421764c639a
rootFaheem <faheemtech1@gmail.com>
2021-02-09 23:44:22 +0530

    perseverance points added

--- lib/app/modules/mars/widgets/mars_perseverance_rover.dart
+++ lib/app/modules/mars/widgets/mars_perseverance_rover.dart
@@ -4,9 +4,33 @@ class MarsPerseveranceRover extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        child: Text("Perseverance rover goes here..."),
-      ),
-    );
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+          Container(
+            child: Text(
+              "What is Perseverance?",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 18.0),
+                color: Color(0xffC4C4C4),
+                child: SizedBox(height: 74.0, width: 4.0),
+              ),
+              Container(
+                width: 310.0,
+                margin: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  "Perseverance is a rover that is on its way to Mars. It will study a a region of Mars called Jezero Crater. This rover will answer lots of questions about the Red Planet and search for signs of past microbial life.",
+                  style: TextStyle(fontSize: 14.0),
+                ),
+              ),
+            ],
+          ),
+        ]));
   }
 }
