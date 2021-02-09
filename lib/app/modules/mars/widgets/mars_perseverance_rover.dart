05f0b79897b2e41604daf246d454ddff50bd9bf0
rootFaheem <faheemtech1@gmail.com>
2021-02-10 00:12:46 +0530

    point -> helicopter

--- lib/app/modules/mars/widgets/mars_perseverance_rover.dart
+++ lib/app/modules/mars/widgets/mars_perseverance_rover.dart
@@ -199,6 +199,30 @@ class MarsPerseveranceRover extends StatelessWidget {
               ),
             ],
           ),
+          Container(
+            child: Text(
+              "First Helicopter",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 18.0),
+                color: Color(0xffC4C4C4),
+                child: SizedBox(height: 74.0, width: 4.0),
+              ),
+              Container(
+                width: 310.0,
+                margin: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  "Joining the Perseverance rover on Mars is the first helicopter designed to fly on another planet. Dubbed Ingenuity, the Mars Helicopter is a technology demonstration that will be the first test of powered flight on another planet",
+                  style: TextStyle(fontSize: 14.0),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
